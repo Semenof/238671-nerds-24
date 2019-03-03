9c6292b86bd002d1c9d3bfc4d624b9ce742b5240
Semenof <semenoff2015@icloud.com>
2019-03-03 11:10:07 +0300

    предзащита

--- js/nerds.js
+++ js/nerds.js
@@ -2,10 +2,10 @@
 var popup = document.querySelector(".modal-write-us");
 var close =document.querySelector(".cross");
 
-// popup.addEventListener("click", function (evt) {
-//     evt.preventDefault();
-//     popup.classList.add("modal-show");
-// });
+//  popup.addEventListener("click", function (evt) {
+//      evt.preventDefault();
+//      popup.classList.add("modal-show");
+//  });
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
